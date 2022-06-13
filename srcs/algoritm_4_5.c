5f6fb1241f0dc7bf9b7b0a0d0fe871e6313e93d6
Miguel Silva <miguel1998282@gmail.com>
2022-06-13 22:59:15 +0100

    Algoritm for 4 and 5 not complete

--- srcs/algoritm_4_5.c
+++ srcs/algoritm_4_5.c
@@ -6,7 +6,7 @@
 /*   By: mtavares <mtavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 20:13:52 by mtavares          #+#    #+#             */
-/*   Updated: 2022/06/13 22:38:25 by mtavares         ###   ########.fr       */
+/*   Updated: 2022/06/13 22:58:39 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,10 @@ void	algoritm_4_5(t_list **a, t_list **b)
 	have_max_min(*a, &max, &min);
 	while ((*a)->content != min)
 		rotate_a(a);
-	if ((*a)->content == min)
-		push_b(b, a);
+	push_b(b, a);
 	while ((*a)->content != max)
 		rotate_a(a);
-	if ((*a)->content == max)
-		push_b(b, a);
+	push_b(b, a);
 	algorithm_3(a);
 	push_a(a, b);
 	rotate_a(a);
